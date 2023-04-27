be1083e632def01be346ef62f43e071613373480
hind <bouhnichahind@gmail.com>
2023-04-26 23:37:33 -0700

    functions commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,23 +5,23 @@
 */
 void jack_bauer(void)
 {
-int hour, day;
+int hour, minut;
 
 
-day = 0;
+hour = 0;
 
 while (hour < 24)
 {
-day = 0;
-while (day < 60)
+minut = 0;
+while (minut < 60)
 {
 _putchar((hour / 10) + '0');
 _putchar((hour % 10) + '0');
 _putchar(':');
-_putchar((day / 10) + '0');
-_putchar((day % 10) + '0');
+_putchar((minut / 10) + '0');
+_putchar((minut % 10) + '0');
 _putchar('\n');
-day++;
+minut++;
 }
 hour++;
 }
