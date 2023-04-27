177b3468eb1c4f3b8d5e7e310c2f56dd0a760e88
hind <bouhnichahind@gmail.com>
2023-04-26 23:29:16 -0700

    functions commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* jack_bauer - prints every minute of the day of Jack Bauer
+* starting from 00:00 to 23:59
+*/
+void jack_bauer(void)
+{
+int hour, day;
+
+
+day = 0;
+
+while (hour < 24)
+{
+day = 0;
+while (day < 60)
+{
+_putchar((hour / 10) + '0');
+_putchar((hour % 10) + '0');
+_putchar(':');
+_putchar((day / 10) + '0');
+_putchar((day % 10) + '0');
+_putchar('\n');
+day++;
+}
+hour++;
+}
+}
